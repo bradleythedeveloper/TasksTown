271b452e947b14e612e71136ae83f0582496a281
BradsTheDeveloper <bradleyaustin2611@gmail.com>
2024-10-25 11:42:47 +0100

    Initial Commit

---
+++ TasksTown/TasksTownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksTownApp.swift
+//  TasksTown
+//
+//  Created by Bradley Austin on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TasksTownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
